6ed776e508c64e8c75160d67b781e0577284d611
MTipuric <118455254+MTipuric@users.noreply.github.com>
2025-07-11 15:57:49 +0100

    create scenario basic page

--- digitalTwin/routes/createScenario.py
+++ digitalTwin/routes/createScenario.py
@@ -3,4 +3,4 @@ from flask import render_template
 
 @bp.route("/createscenario")
 def createScenario():
-    return render_template("temp_page.html")
+    return render_template("create_scenario.html")
