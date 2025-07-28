bcebe30a2c99bf0fe689110643265da3e45f759f
MTipuric <118455254+MTipuric@users.noreply.github.com>
2025-07-28 10:51:31 +0100

    Checkpoint
    Minor changes. Comiting this for now, so I can focus on integrating the ABM code

--- digitalTwin/routes/reports.py
+++ digitalTwin/routes/reports.py
@@ -5,22 +5,21 @@ from pathlib import Path
 
 @bp.route('/reports', methods = ['GET'])
 def reports():
-    filepath = Path(__file__).parents[1] /"data/geo_data/example2.json"
+    filepath = Path(__file__).parents[1] /"data/geo_data/metadata.json"
     data = getData.loadJSONdata(filepath)
     return render_template("reports.html", data = data)
 
 @bp.route('/reports/<ID>', methods = ['GET'])
 def specific_report(ID):
-    data = findData(ID)
-    return render_template("reportTemplate.html", data = data)
+    return render_template("reportTemplate.html", summaryGIS = findData(ID, "summaryGIS.json"))
 
-def findData(ID):
+def findData(ID, filename):
     filepath = Path(__file__).parents[1] /"data/geo_data/metadata.json"
     metadata = getData.loadJSONdata(filepath)
     for item in metadata["files"]:
         if item["id"] == ID:
            data_loc = item["data_loc"]
-           filepath = Path(__file__).parents[1] /"data/geo_data" / data_loc
+           filepath = Path(__file__).parents[1] /"data/geo_data" / data_loc / filename
            data = getData.loadJSONdata(filepath)
            break
     return data
