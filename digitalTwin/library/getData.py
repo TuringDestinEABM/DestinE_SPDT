e56896fdba1c1468d088ee6b822b6d60dc8c54f2
MTipuric <118455254+MTipuric@users.noreply.github.com>
2025-11-13 14:55:45 +0000

    Moved paths to config
    Temporary until prop databasing set up

--- digitalTwin/library/getData.py
+++ digitalTwin/library/getData.py
@@ -1,6 +1,7 @@
 '''Helper scripts for loading in different data'''
 import json
 from pathlib import Path
+from flask import current_app
 
 def loadJSONdata(filepath):
     with open(filepath) as file:
@@ -20,13 +21,14 @@ def findGEOData(ID, filename):
     # TODO: make this do a 404            
 
 def findMetadata(ID):
-    path = Path(__file__).parents[1] /"data/geo_data/results" / ID / "metadata.json"
+    results_dir = Path(current_app.config['RESULTS_DIR'])
+    path = results_dir / ID / "metadata.json"
     metadata = loadJSONdata(path)
-    
     return metadata
 
 def listSummaryFigures(ID):
-    path = Path(__file__).parents[1] /"data/geo_data/results" / ID
+    results_dir = Path(current_app.config['RESULTS_DIR'])
+    path = results_dir / ID
     figures = dict(plot_day_hour = str(path /"plot_day_hour.png"),
                    plot_hexbin = str(path /"plot_day_hour.png"),
                    plot_prop_type = str(path /"plot_prop_type.png"),
